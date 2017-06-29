18dcb8e6bf1f0ff03d1a3c66645ce09be0d595e5
Frank <francisw396@gmail.com>
2017-06-29 14:15:18 -0400

    game is functional but not session

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ const data = require('./models/data.js')
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(expressValidator());
 
 app.use(session({
+  // genid: function(req),
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
@@ -23,95 +26,110 @@ app.set('view engine', 'mustache');
 app.set('views', './views');
 
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 
   req.session.numberOfGuessesLeft = 8;
 
-  if(req.session){
+  if (req.session) {
     res.redirect('/theGame/');
   } else {
     res.send("we are communicating");
   }
 
 });
-app.get('/theGame/', function(req, res){
+app.get('/theGame/', function(req, res) {
 
   let bullshitArray = [req.session.numberOfGuessesLeft];
   console.log(req.session.numberOfGuessesLeft);
-  res.render('theGame', {correctGuessesArray: data.correctGuessesArray, guessesArray: data.guessesArray, bullshitArray: bullshitArray});
+  res.render('theGame', {
+    correctGuessesArray: data.correctGuessesArray,
+    guessesArray: data.guessesArray,
+    bullshitArray: bullshitArray
+  });
 });
-app.post('/theGame/', function(req, res){
+app.post('/theGame/', function(req, res) {
   console.log(data.correctWord);
-  let userGuess = req.body.userGuess;
+  let userGuess = req.body.userGuess.toLowerCase();
   req.checkBody("userGuess", "You must enter a guess").notEmpty();
   var errors = req.validationErrors();
 
-  if(errors){
+  if (errors) {
     // res.send(errors[0].msg);
     res.redirect('/blankEntry/');
   } else {
-    if (userGuess.length > 1){
+    if (userGuess.length > 1) {
       // res.send("your entry must be one letter");
       res.redirect('/tooManyCharacters/');
-    } else if (data.testingForDuplicateInput(userGuess)){
+    } else if (data.testingForDuplicateInput(userGuess)) {
       res.redirect('/duplicateEntry');
     } else {
-        data.guessesArray.push(userGuess);
-        data.isUserGuessCorrect(req, userGuess);
-        let correctGuesses = req.session.numberOfCorrectGuesses;
-        let correctArray = data.correctGuessesArray;
-        let winner = data.didUserWin(correctArray);
-        let guessesLeft = req.session.numberOfGuessesLeft;
-        bullshitArray = [req.session.numberOfGuessesLeft]
-        if (guessesLeft <= 0){
-          res.redirect('/youLose/');
-        } else if (winner){
-          res.redirect('/youWin/');
-          console.log(data.correctWordWrapper);
-        } else {
-          res.redirect('/theGame/');
-        }
+      data.guessesArray.push(userGuess);
+      data.isUserGuessCorrect(req, userGuess);
+      let correctGuesses = req.session.numberOfCorrectGuesses;
+      let correctArray = data.correctGuessesArray;
+      let winner = data.didUserWin(correctArray);
+      let guessesLeft = req.session.numberOfGuessesLeft;
+      bullshitArray = [req.session.numberOfGuessesLeft]
+      if (guessesLeft <= 0) {
+        res.redirect('/youLose/');
+      } else if (winner) {
+        res.redirect('/youWin/');
+      } else {
+        res.redirect('/theGame/');
+      }
       // res.redirect('/theGame/');
     }
   }
 });
-app.get('/youLose/', function(req, res){
-  res.render('youLose', {correctWordWrapper: data.correctWordWrapper});
+app.get('/youLose/', function(req, res) {
+  req.session.destroy();
+  res.render('youLoseTest', {
+    correctWordWrapper: data.correctWordWrapper,
+    correctGuessesArray: data.correctGuessesArray,
+    guessesArray: data.guessesArray,
+    bullshitArray: bullshitArray
+  });
 });
-app.get('/youWin/', function(req, res){
-  res.render('youWin', {correctWordWrapper: data.correctWordWrapper});
+app.get('/youWin/', function(req, res) {
+  req.session.destroy();
+  res.render('youWinTest', {
+    correctWordWrapper: data.correctWordWrapper,
+    correctGuessesArray: data.correctGuessesArray,
+    guessesArray: data.guessesArray,
+    bullshitArray: bullshitArray
+  });
 });
-app.get('/soreLoser/', function(req, res){
+app.get('/soreLoser/', function(req, res) {
   res.send('something went wrong');
 });
-app.post('/soreLoser/', function(req, res){
+app.post('/soreLoser/', function(req, res) {
   res.send("I guess you're a sore loser");
 });
-app.get('/playAgain/', function(req, res){
+app.get('/playAgain/', function(req, res) {
   res.send('something went wrong');
 });
-app.post('/playAgain/', function(req, res){
-  console.log(req.session);
+app.post('/playAgain/', function(req, res) {
+  console.log(req.session.numberOfGuessesLeft);
   res.redirect('/');
 });
-app.get('/tooManyCharacters/', function(req, res){
+app.get('/tooManyCharacters/', function(req, res) {
   res.render('moreThanOneCharacter');
 });
-app.post('/tooManyCharacters/', function(req, res){
+app.post('/tooManyCharacters/', function(req, res) {
   res.redirect('/theGame/');
 });
-app.get('/duplicateEntry/', function(req, res){
+app.get('/duplicateEntry/', function(req, res) {
   res.render('duplicateEntry');
 });
-app.post('/duplicateEntry/', function(req, res){
+app.post('/duplicateEntry/', function(req, res) {
   res.redirect('/theGame/');
 });
-app.get('/blankEntry/', function(req, res){
+app.get('/blankEntry/', function(req, res) {
   res.render('blankEntry');
 });
-app.post('/blankEntry/', function(req, res){
+app.post('/blankEntry/', function(req, res) {
   res.redirect('/theGame/');
 });
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log('Successfully started express application!');
 });
